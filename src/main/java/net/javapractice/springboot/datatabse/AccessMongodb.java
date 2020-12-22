8dff348bd8ca2aadbaf57c46f1fee921e0bb08a1
Hoang Le <hoang.le4@pcc.edu>
2020-12-21 21:38:36 -0800

    remove Main application

--- src/main/java/net/javapractice/springboot/datatabse/AccessMongodb.java
+++ src/main/java/net/javapractice/springboot/datatabse/AccessMongodb.java
@@ -4,12 +4,9 @@ package net.javapractice.springboot.datatabse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import net.javapractice.spingboot.model.User;
 import net.javapractice.spingboot.repository.UserRepository;
 
-@SpringBootApplication
 public class AccessMongodb implements CommandLineRunner {
 
 	@Autowired
